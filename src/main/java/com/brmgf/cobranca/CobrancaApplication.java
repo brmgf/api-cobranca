cacbfc47de42b58d6b14f6f146c9deebd7eae98c
brmgf <a16010@bcc.unifal-mg.edu.br>
2021-02-16 17:13:23 -0300

    ajustando tipos de dados

--- src/main/java/com/brmgf/cobranca/CobrancaApplication.java
+++ src/main/java/com/brmgf/cobranca/CobrancaApplication.java
@@ -1,7 +1,12 @@
 package com.brmgf.cobranca;
 
+
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 @SpringBootApplication
 public class CobrancaApplication {
@@ -10,4 +15,8 @@ public class CobrancaApplication {
 		SpringApplication.run(CobrancaApplication.class, args);
 	}
 
+	@Bean
+	public FixedLocaleResolver localeResolver() {
+		return new FixedLocaleResolver(new Locale("pt", "BR"));
+	}
 }
